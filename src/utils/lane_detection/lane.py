b1989764548e4d2ff6f542aa51a093c8ebff6278
stefke023 <oncstefan@gmail.com>
2025-01-16 13:54:43 +0100

    Fix : Using pixels with calculation

--- src/utils/lane_detection/lane.py
+++ src/utils/lane_detection/lane.py
@@ -1,17 +1,19 @@
 import cv2
 import numpy as np
-
-import src.utils.lane_detection.polynomial_fit as fit
-import src.utils.lane_detection.preprocessing as prep
-
+import preprocessing as prep
+import polynomial_fit as fit
 
 def process(image):
     hsvImage = prep.hsv(image)
-    #contrastImg = prep.increase_contrast(hsvImage)
     cannyFrame = prep.canny(hsvImage, 50, 150)
+    cv2.imshow('canny', cannyFrame)
     warpedFrame, inverseM = prep.warpImage(cannyFrame)
+    
+    osovina = 300 # 300 piksela, inace 0.2m
 
     res = fit.lineFit(warpedFrame)
+    cv2.imshow('Sliding Windows', res['outputImage'])
+    cv2.waitKey(0)
 
     leftFit = res['leftFit']
     rightFit = res['rightFit']
@@ -20,22 +22,10 @@ def process(image):
     leftLaneIndicies = res['leftLaneIndicies']
     rightLaneIndicies = res['rightLaneIndicies']
 
-    leftCurve, rightCurve = fit.calculateCurve(image, leftLaneIndicies, rightLaneIndicies, nonzeroX, nonzeroY)
+    leftCurve, rightCurve = fit.calculateCurve(warpedFrame, leftLaneIndicies, rightLaneIndicies, nonzeroX, nonzeroY)
+    averageCurve = (leftCurve+rightCurve)/2.0
     if leftCurve > rightCurve:
-        angle = fit.calculate_steering_angle(0.2, (leftCurve+rightCurve)/2, False)
+        angle = fit.calculate_steering_angle(osovina, averageCurve, False)
     else:
-        angle = fit.calculate_steering_angle(0.2, (leftCurve + rightCurve) / 2, True)
-
-    bottomY = image.shape[0] - 1
-    bottomXLeft = leftFit[0]*(bottomY**2) + leftFit[1]*bottomY + leftFit[2]
-    bottomXRight = rightFit[0]*(bottomY**2) + rightFit[1]*bottomY + rightFit[2]
-    vehicleOffset = image.shape[1]/2 - (bottomXLeft + bottomXRight)/2
-
-    metersPerPixelX = 0.35/700
-    vehicleOffset *= metersPerPixelX
-
-    effective_radius = (leftCurve + rightCurve) / 2
-
-    result = fit.showResult(image, leftFit, rightFit, inverseM, leftCurve, rightCurve, vehicleOffset)
-
-    return result, angle
+        angle = fit.calculate_steering_angle(osovina, averageCurve, True)
+    print('Angle', angle)
